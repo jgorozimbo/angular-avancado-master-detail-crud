b17f55a95774ce573f0fbd78bb25fa46e8b0e38a
Josemar Orozimbo <jgorozimbo@gmail.com>
2022-01-08 09:35:30 -0300

    Adding RouterModule to SharedModule && correcting breadCrumbComponent && improving page components with breadCrumbComponent

--- src/app/shared/components/bread-crumb/bread-crumb.component.ts
+++ src/app/shared/components/bread-crumb/bread-crumb.component.ts
@@ -12,7 +12,7 @@ interface BreadCrumbItem {
 })
 export class BreadCrumbComponent implements OnInit {
 
-  @Input() itens: Array<BreadCrumbItem> = [];
+  @Input() items: Array<BreadCrumbItem> = [];
 
   constructor() { }
 
@@ -20,8 +20,8 @@ export class BreadCrumbComponent implements OnInit {
   }
 
   isTheLastItem(item: BreadCrumbItem): boolean {
-    const index = this.itens.indexOf(item);
-    return index + 1 == this.itens.length;
+    const index = this.items.indexOf(item);
+    return index + 1 == this.items.length;
   }
 
 }
