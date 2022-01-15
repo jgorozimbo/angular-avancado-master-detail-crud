e13862e576b775521721eb4a24472a03ce58202a
Josemar Orozimbo <jgorozimbo@gmail.com>
2022-01-15 12:27:32 -0300

    improving Reports

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from '../../shared/shared.module'
+import { ChartModule} from "primeng/chart"
+
+import { SharedModule } from '../../shared/shared.module';
 
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
@@ -9,7 +11,8 @@ import { ReportsComponent } from './reports/reports.component';
   declarations: [ReportsComponent],
   imports: [
     SharedModule,
-    ReportsRoutingModule
+    ReportsRoutingModule,
+    ChartModule
   ]
 })
 export class ReportsModule { }
